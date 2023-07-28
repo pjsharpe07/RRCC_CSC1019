5602bb6dd640c0a47e4ade9bd5fdfe8b5716ac61
Preston Sharpe <psharpe@xentity.com>
2023-07-28 10:38:02 -0600

    Topic 4 - continuing to work

---
+++ Topic 4/arithmetic.py
@@ -0,0 +1,30 @@
+########################################
+##### adv. arithmetic operations #######
+########################################
+
+# floor division
+
+# my_num = int(input('What number do you want to try? '))
+
+# normal_division = my_num / 11
+# floor_division = my_num // 11
+
+# # input of 64
+# print(normal_division) # 5.818181818181818
+# print(floor_division) # 5
+
+# students should try this
+# my_num = 100
+# divisor = 16
+
+# my_total = my_num // divisor
+
+# print(my_total)
+
+#### let's learn about modulo
+
+# print(24 % 10) # 4
+# print(50 % 50) # 0
+# print(1 % 2) #  1
+# print(53 % 100) # 53
+
